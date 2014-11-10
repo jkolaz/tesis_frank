f31296668089e8e9d2acc5ca830f4d7aaaaa9555
Julio Salsavilca <j.salsavilca@gmail.com>
2014-11-10 16:44:52 -0500

    archivos de configuracion

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://www.jkolaz.com/frank';
+$config['base_url']	= 'http://localhost/frank';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'bvtyslko_frank';
-$db['default']['password'] = 'U#2Lb5mxHVFB';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
 $db['default']['database'] = 'bvtyslko_frank';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
